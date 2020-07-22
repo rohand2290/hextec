e68166af3b0af5b5b5ef585cb62963b6934b946f
Rohan Deshpande <Rohan>
2020-07-22 11:21:40 -0400

    added parameter and body parsing functionality

--- src/HextecCreator.ts
+++ src/HextecCreator.ts
@@ -12,41 +12,44 @@ class HextecCreator {
       run: (port: number) => {
         http
           .createServer(function (req, res) {
-            var correctRoute;
-            for (var route of routes) {
-              if (route.getUrl() === req.url) {
-                correctRoute = route;
-                break;
-              } else {
-                correctRoute = "Route not found";
-              }
-            }
-
-            if (typeof correctRoute != "object") {
-              res.write("Route not found");
-              res.end();
-            } else {
-              let data: any = [];
-              let parsedData = {};
-              let isDone = false;
-              req.on("data", (chunk) => data.push(chunk));
-              req.on("end", () => {
-                parsedData = JSON.parse(data);
+            let data: any = [];
+            let correctRoute: any;
+            req
+              .on("data", (chunk) => {
+                data.push(chunk);
+                for (var route of routes) {
+                  if (
+                    route.getUrl() ===
+                    url.parse(req.url as string, true).pathname
+                  ) {
+                    correctRoute = route;
+                    break;
+                  } else {
+                    correctRoute = "Route not found";
+                  }
+                }
+              })
+              .on("end", () => {
+                data = Buffer.concat(data).toString();
+                if (typeof correctRoute != "object") {
+                  res.write("Route not found");
+                  res.end();
+                } else {
+                  res.write(
+                    correctRoute
+                      .getHandlerFunc()(
+                        new HTTPRequest(
+                          req.method,
+                          req.url,
+                          url.parse(req.url as string, true).query,
+                          JSON.parse(data)
+                        )
+                      )
+                      .getResponse()
+                  );
+                  res.end();
+                }
               });
-              res.write(
-                correctRoute
-                  .getHandlerFunc()(
-                    new HTTPRequest(
-                      req.method,
-                      req.url,
-                      url.parse(req.url as string, true).query,
-                      parsedData
-                    )
-                  )
-                  .getResponse()
-              );
-              res.end();
-            }
           })
           .listen(port);
       },
